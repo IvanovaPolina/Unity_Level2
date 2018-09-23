9199260ae6e7cac19b24d17c6ba433ff2df8e920
IvanovaPolina <ip.96@mail.ru>
2018-09-24 02:20:29 +0400

    Что-то напутала с прошлым коммитом/ревертом, подтягиваю упущенное. Пока еще не привыкла к Git'у.

---
+++ Assets/Homework/Scripts/Selection/SelectionModel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Homework
+{
+	/// <summary>
+	/// Хранит в себе логику того, как стоит выделять объект
+	/// </summary>
+	public sealed class SelectionModel : BaseSceneObject
+	{
+		public float selectingDistance = 3f;    // Дистанция, на которой игрок будет "видеть" объект
+		/// <summary>
+		/// Обнаруженный игроком объект
+		/// </summary>
+		public ISelectable SelectedObject
+		{
+			get {
+				if (selectedObj != null) return selectedObj;
+				else return null;
+			}
+		}
+
+		private ISelectable selectedObj;
+		private RaycastHit hit;
+
+		/// <summary>
+		/// Проверяет, есть ли впереди объект
+		/// </summary>
+		public void Linecast() {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit, selectingDistance))
+				selectedObj = hit.transform.GetComponent<ISelectable>();
+		}
+
+		/// <summary>
+		/// Взаимодействует с объектом
+		/// </summary>
+		public void Interact() {
+			if (selectedObj != null) selectedObj.Interact();
+		}
+	}
+}
