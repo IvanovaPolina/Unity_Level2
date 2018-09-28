0e8641bd63d79c724cc06ea326c5edd8baf1cb77
IvanovaPolina <ip.96@mail.ru>
2018-09-29 02:00:22 +0400

    Сделала 3 задание. Реализовала движение тиммэйта по точкам. Переделала классы TeammateModel и TeammateController для этой цели, а также добавила класс HitPointsView для отображения расставленных точек. При назначении новой точки, если кликнуть на горы, он почему-то попадает рэйкастом в уничтожающие триггеры вокруг уровня за горами и удаляет тиммэйта, даже не двигая его. Для того, чтобы тиммэйт не уничтожался, оградила карту стенами, которые запекла с маской Not Walkable, а у тиммэйта в NavMeshAgent убрала эту маску. В скрипте модели (52 строка) в NavMesh.SamplePosition соответственно выставила маску из NavMeshAgent'а тиммэйта.

--- Assets/Homework/Scripts/Teammate/TeammateController.cs
+++ Assets/Homework/Scripts/Teammate/TeammateController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Homework
@@ -10,12 +11,17 @@ namespace Homework
 		/// </summary>
 		public static UnityAction<TeammateModel> OnTeammateSelected;
 		private TeammateModel currentTeammate;
+		
+		public static UnityAction<Vector3, TeammateModel> OnHitPointApplied;
+		public static UnityAction<TeammateModel> OnHitPointDeleted;
 
 		/// <summary>
 		/// Выбирает текущего тиммэйта
 		/// </summary>
 		public void SelectTeammate(TeammateModel teammate) {
+			if (teammate == currentTeammate) return;	// не выбираем одного и того же тиммэйта несколько раз
 			currentTeammate = teammate;
+			StartCoroutine(SetAllDestinations(currentTeammate));
 			if (OnTeammateSelected != null)
 				OnTeammateSelected.Invoke(currentTeammate);
 		}
@@ -28,16 +34,31 @@ namespace Homework
 		}
 
 		/// <summary>
-		/// Выбирает тиммэйта, а также даёт ему команду следовать в позицию на карте
+		/// Выбирает нового тиммэйта, либо добавляет текущему позицию для передвижения
 		/// </summary>
 		public void MoveCommand() {
 			RaycastHit hit;
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if(Physics.Raycast(ray, out hit)) {		// в PlayerModel указать переменную "maxDistanceToControlTeammate". здесь третьим параметром передавать эту переменную
 				TeammateModel teammate = hit.collider.GetComponent<TeammateModel>();
-				if (teammate) SelectTeammate(teammate);		// если луч попал в teammate - выбираем его
-				else if (currentTeammate)	// иначе: если есть выбранный teammate - луч попал в неживой объект
-					currentTeammate.SetDestination(hit.point);	// и teammate будет бежать туда
+				if (teammate) SelectTeammate(teammate);     // если луч попал в teammate - выбираем его
+				else if (currentTeammate) { // иначе: если есть выбранный teammate - луч попал в неживой объект
+					currentTeammate.EnqueueNewPoint(hit.point);   // добавляем точку назначения в очередь
+					OnHitPointApplied.Invoke(hit.point, currentTeammate);	// сообщаем view позицию новой точки
+				}
+			}
+		}
+
+		/// <summary>
+		/// Реализует движение тиммэйта по точкам
+		/// </summary>
+		/// <param name="teammate">Выбранный тиммэйт</param>
+		IEnumerator SetAllDestinations(TeammateModel teammate) {
+			while (teammate != null) {  // пока тиммэйт жив
+				yield return new WaitUntil(() => teammate.HitPointsCount > 0);  // ждем до тех пор, пока не появятся точки назначения
+				yield return new WaitUntil(() => teammate.IsSetDestination);  // ждем, пока он не придет в последнюю точку назначения
+				teammate.SetDestination();    // говорим тиммэйту двигаться к следующей точке
+				OnHitPointDeleted.Invoke(teammate);	// убираем из поля зрения предыдущую точку
 			}
 		}
 	}
--- Assets/Homework/Scripts/Teammate/TeammateModel.cs
+++ Assets/Homework/Scripts/Teammate/TeammateModel.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
@@ -8,8 +9,12 @@ namespace Homework
 	{
 		private NavMeshAgent agent;
 		private ThirdPersonCharacter character;
-		private Vector3 currentPos;
 		private bool followPlayer;
+		private Queue<Vector3> hitPoints = new Queue<Vector3>();
+		/// <summary>
+		/// Возвращает количество точек назначения в пути тиммэйта
+		/// </summary>
+		public int HitPointsCount { get { return hitPoints.Count; } }
 
 		private void Start() {
 			agent = GetComponentInChildren<NavMeshAgent>();
@@ -17,8 +22,6 @@ namespace Homework
 
 			agent.updateRotation = false;
 			agent.updatePosition = true;
-
-			currentPos = Position;
 		}
 
 		private void Update() {
@@ -30,17 +33,35 @@ namespace Homework
 			else
 				character.Move(Vector3.zero, false, false);
 		}
-		
+
+		/// <summary>
+		/// Добавляет новую точку назначения в путь тиммэйта
+		/// </summary>
+		public void EnqueueNewPoint(Vector3 point) {
+			hitPoints.Enqueue(point);
+		}
+
 		/// <summary>
 		/// Задаёт координаты, в которые должен попасть тиммэйт
 		/// </summary>
 		/// <param name="pos">Координаты</param>
-		public void SetDestination(Vector3 pos) {
+		public void SetDestination() {
+			if (hitPoints.Count == 0) return;
+			Vector3 pos = hitPoints.Dequeue();
 			NavMeshHit hit;
-			if (NavMesh.SamplePosition(pos, out hit, 50f, -1)) {
+			if (NavMesh.SamplePosition(pos, out hit, 50f, agent.areaMask)) {
 				followPlayer = false;
 				agent.SetDestination(hit.position);
 			} else Debug.Log("Wrong position!");
+		}
+
+		/// <summary>
+		/// Определяет, достиг ли тиммэйт заданной позиции
+		/// </summary>
+		public bool IsSetDestination {
+			get {
+				return agent.remainingDistance < agent.stoppingDistance;
+			}
 		}
 
 		/// <summary>
