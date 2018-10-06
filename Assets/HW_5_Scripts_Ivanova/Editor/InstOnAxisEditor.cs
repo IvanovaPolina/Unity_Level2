6152cb04e9b1dff652e3454d4b758dccb85d4c4e
IvanovaPolina <ip.96@mail.ru>
2018-10-06 14:06:19 +0400

    Сделала все задания. Заметки: 1. Разработать свое окно редактора, добавить в него базовые составляющие интерфейса. - Скрипт MyWindow в папке Editor. 2. Реализовать вызов разработанного окна с помощью меню. - В скрипте MyWindow, в методе ShowWindow(). 3. Расширить функционал скрипта в окне инспектора. - Сделано в классах InstOnAxis и InstOnAxisEditor. 4. Придумать и реализовать алгоритм создания уникальных имен у объектов на сцене. - Моя совесть чиста) Разобрала пример в методичке. 5. Придумать и реализовать алгоритм очистки сцены от объектов. - В скрипте MyWindow добавлены две кнопки: "Удалить последний созданный объект" и "Удалить все созданные объекты". В скрипте InstOnAxisEditor это тоже реализовано. 6. Добавить еще несколько свойств объекта для сохранения. - Добавлены: цвет, слой, тип коллайдера объекта. 7. *Разработать свой алгоритм расстановки объектов. - В скрипте MyWindow реализовано создание спирали настраиваемой плотности, с настраиваемым количеством витков. Для наглядности в поле ObjectInstantiate можно поставить куб с rotation(45, 45, 45) - так смотрится эффектнее. Не ломала голову над чем-то более интересным, т.к. решила больше времени уделить контенту игры.

---
+++ Assets/HW_5_Scripts_Ivanova/Editor/InstOnAxisEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(InstOnAxis))]
+public class InstOnAxisEditor : Editor
+{
+	public override void OnInspectorGUI() {
+		//DrawDefaultInspector();
+		InstOnAxis instOnAxis = (InstOnAxis)target;
+		Init();
+
+		GUILayout.BeginHorizontal();
+		bool createButton = GUILayout.Button("Создать объекты", EditorStyles.miniButtonLeft, GUILayout.MinHeight(10), GUILayout.MaxHeight(20));
+		GUILayout.FlexibleSpace();
+		bool destroyButton = GUILayout.Button("Удалить объекты", EditorStyles.miniButtonRight, GUILayout.MinHeight(10), GUILayout.MaxHeight(20));
+		GUILayout.EndHorizontal();
+
+		if (createButton) {
+			instOnAxis.CreateObj();
+			EditorWindow.mouseOverWindow.ShowNotification(new GUIContent("Все объекты созданы"));
+		}
+
+		if (destroyButton) {
+			if (EditorUtility.DisplayDialog("Удаление объектов", "Вы уверены, что хотите удалить все созданные объекты?", "Да", "Нет")) {
+				instOnAxis.DestroyObj();
+				EditorWindow.mouseOverWindow.ShowNotification(new GUIContent("Созданные объекты удалены"));
+			}
+		}
+	}
+
+	/// <summary>
+	/// Инициализирует и отображает поля скрипта
+	/// </summary>
+	private void Init() {
+		SerializedProperty _countProp = serializedObject.FindProperty("_count");
+		EditorGUILayout.IntSlider(_countProp, 2, 50, "Количество объектов");   // для слайдера
+		//EditorGUILayout.PropertyField(_countProp, new GUIContent("Количество объектов"));	// для целочисленного значения
+		SerializedProperty _offsetProp = serializedObject.FindProperty("_offset");
+		EditorGUILayout.IntSlider(_offsetProp, -3, 3, "Отступ");
+		SerializedProperty _objProp = serializedObject.FindProperty("_obj");
+		EditorGUILayout.PropertyField(_objProp, new GUIContent("Префаб", "Префаб объекта, который хотим создать"));
+		SerializedProperty _axisProp = serializedObject.FindProperty("_axis");
+		EditorGUILayout.PropertyField(_axisProp, new GUIContent("Ось", "Ось, вдоль которой нужно создать объекты"));
+		SerializedProperty _offsetFromAxisProp = serializedObject.FindProperty("offsetFromAxis");
+		_offsetFromAxisProp.vector3IntValue = EditorGUILayout.Vector3IntField("Смещение от оси", _offsetFromAxisProp.vector3IntValue);
+
+		serializedObject.ApplyModifiedProperties(); // сохраняем изменения введенных значений
+	}
+}
---
+++ Assets/HW_5_Scripts_Ivanova/Serialization/SerializableGameObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SerializableGameObject
+{
+	public string name;
+	public Vector3 position;
+	public Quaternion rotation;
+	public Vector3 scale;
+	public Color color;
+	public int layer;
+	public string colliderType;
+}
